950841b9e34e5870d283a244041bc78c3e59ea26
Anupa095 <anupaperera95@gmail.com>
2025-08-06 12:43:30 +0530

    initial files

---
+++ main.py
@@ -0,0 +1,55 @@
+print("Anupa")
+x=100
+print ("my age",x)
+#ggggggggg
+
+
+
+
+print(7+3)
+print(type(6.66))
+print(10%2==0)
+print("AI"+"Chatbot")
+
+i=7
+if(i<15):
+    print("i is smaller 15")
+    print("im in if Block")
+
+else:
+    print("i is grater than 15")
+    print("im in else Block")
+    print("im not in if and not in else Block")
+
+
+
+i = 20
+
+if i == 10:
+    print("i is 10")
+elif i == 15:
+    print("i is 15")
+elif i == 20:
+    print("i is 20")
+else:
+    print("i is not present")
+
+
+
+i=1
+while i<6:
+    print(i)
+    i+=1
+
+
+fruit=["apple","banana","cherry"]
+for x in fruit:
+    print(x)
+
+
+#marks=int(input("Enter your marks: "))
+#print(type(marks))
+
+marks1=int(input("Enter your marks: "))
+marks2=int(input("Enter your marks: "))
+print(marks1+marks2)
